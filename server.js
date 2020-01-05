2a0d3d02bc5a5bd35a6553ad997e42a11fe0f068
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-05 17:00:45 +0200

    intialize server.js

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const logger = require('./utils/logger')('Server');
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+   bodyParser.urlencoded({
+      extended: false,
+   }),
+);
+
+morgan((tokens, req, res) =>
+   [
+      `<${process.env.NODE_ENV}>`,
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+   ].join(' '),
+);
+
+// Unhandled Promise Rejection Handler
+process.on('unhandledRejection', ex => {
+   logger.error(ex.message, ex);
+   app.use((_req, res) => {
+      res.status(500).json({
+         err: null,
+         msg: '500 Internet Error',
+         data: null,
+      });
+   });
+   process.exit(1);
+});
+
+// Port
+const port = process.env.PORT || '3000';
+
+// Listen
+app.listen(port, () => {
+   logger.log(`App Listen Successfully To Port ${port}`);
+});
