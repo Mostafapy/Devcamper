3cc3bdddef272163cd6828e9b75e43bf3557b830
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-06 13:52:55 +0200

    add mongo connections to server.js

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const bodyParser = require('body-parser');
 
 const logger = require('./utils/logger')('Server');
 
+// DB
+const connectDB = require('./config/db');
+
 // Routes
 const routes = require('./routes/index');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// connect to MongoDB
+connectDB();
 
 const app = express();
 
@@ -34,25 +40,27 @@ morgan((tokens, req, res) =>
    ].join(' '),
 );
 
-// Unhandled Promise Rejection Handler
-process.on('unhandledRejection', ex => {
-   logger.error(ex.message, ex);
-   app.use((_req, res) => {
-      res.status(500).json({
-         err: null,
-         msg: '500 Internet Error',
-         data: null,
-      });
-   });
-   process.exit(1);
-});
-
 // Mount the routes
 app.use(routes);
 // Port
 const port = process.env.PORT || '3000';
 
 // Listen
-app.listen(port, () => {
-   logger.log(`App Listen Successfully To Port ${port}`);
+const server = app.listen(port, () =>
+   logger.log(`App Listen Successfully To Port ${port}`),
+);
+
+// Unhandled Promise Rejection Handler
+process.on('unhandledRejection', ex => {
+   logger.error(ex.message, ex);
+   app.use((_req, res) => {
+      res.status(500).json({
+         err: true,
+         msg: '500 Internet Error',
+         data: null,
+      });
+   });
+
+   // eslint-disable-next-line implicit-arrow-linebreak
+   server.close(() => process.exit(1));
 });
