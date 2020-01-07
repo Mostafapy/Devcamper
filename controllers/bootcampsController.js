0f28b5133e7144b27d13f3aa55e562b207f2c2bf
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-07 18:50:42 +0200

    add asyncHandler to bootcamps controller

--- controllers/bootcampsController.js
+++ controllers/bootcampsController.js
@@ -1,11 +1,13 @@
 const BootcampModel = require('./../models/Bootcamp');
 const ErrorResponse = require('./../utils/errorResponse');
+const asyncHandler = require('./../middlewares/asyncHandler');
+
 const logger = require('./../utils/logger')('Controllers:BootcampsController');
 // @desc Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access Public
-const getBootcamps = async (_req, res) => {
-   try {
+const getBootcamps = asyncHandler(
+   async (_req, res) => {
       const bootcamps = await BootcampModel.find();
 
       res.status(200).json({
@@ -13,20 +15,16 @@ const getBootcamps = async (_req, res) => {
          count: bootcamps.length,
          data: bootcamps,
       });
-   } catch (err) {
-      logger.error('@getBootcamps() [error: %s]'.red, err.message);
-
-      res.status(400).json({
-         success: false,
-      });
-   }
-};
+   },
+   logger,
+   '@getBootcamps() [error: %s]'.red,
+);
 
 // @desc Get a single bootcamps by id
 // @route GET /api/v1/bootcamps/:id
 // @access Public
-const getBootcampById = async (req, res, next) => {
-   try {
+const getBootcampById = asyncHandler(
+   async (req, res, next) => {
       const bootcamp = await BootcampModel.findById(req.params.id);
 
       // bootcamp doesn't exist
@@ -45,42 +43,32 @@ const getBootcampById = async (req, res, next) => {
          success: true,
          data: bootcamp,
       });
-   } catch (err) {
-      err.loggerObject = {
-         logger,
-         loggerMessage: '@getBootcampById() [error: %s]'.red,
-      };
-
-      next(err);
-   }
-};
+   },
+   logger,
+   '@getBootcampById() [error: %s]'.red,
+);
 
 // @desc Create new bootcamp
 // @route POST /api/v1/bootcamps
 // @access Private
-const createBootcamp = async (req, res, next) => {
-   try {
+const createBootcamp = asyncHandler(
+   async (req, res) => {
       const newBootcamp = await BootcampModel.create(req.body);
 
       res.status(201).json({
          success: true,
          data: newBootcamp,
       });
-   } catch (err) {
-      err.loggerObject = {
-         logger,
-         loggerMessage: '@createBootcamp() [error: %s]'.red,
-      };
-
-      next(err);
-   }
-};
+   },
+   logger,
+   '@createBootcamp() [error: %s]'.red,
+);
 
 // @desc Update a single bootcamp by id
 // @route PUT /api/v1/bootcamps/:id
 // @access Private
-const updateBootcampById = async (req, res, next) => {
-   try {
+const updateBootcampById = asyncHandler(
+   async (req, res, next) => {
       const bootcamp = await BootcampModel.findByIdAndUpdate(
          req.params.id,
          req.body,
@@ -106,21 +94,16 @@ const updateBootcampById = async (req, res, next) => {
          success: true,
          data: bootcamp,
       });
-   } catch (err) {
-      err.loggerObject = {
-         logger,
-         loggerMessage: '@updateBootcampById() [error: %s]'.red,
-      };
-
-      next(err);
-   }
-};
+   },
+   logger,
+   '@updateBootcampById() [error: %s]'.red,
+);
 
 // @desc Delete a single bootcamp by id
 // @route DELETE /api/v1/bootcamps/:id
 // @access Private
-const deleteBootcampById = async (req, res, next) => {
-   try {
+const deleteBootcampById = asyncHandler(
+   async (req, res, next) => {
       const bootcamp = await BootcampModel.findByIdAndDelete(req.params.id);
 
       // bootcamp doesn't exist
@@ -139,15 +122,10 @@ const deleteBootcampById = async (req, res, next) => {
          success: true,
          data: {},
       });
-   } catch (err) {
-      err.loggerObject = {
-         logger,
-         loggerMessage: '@deleteBootcampById() [error: %s]'.red,
-      };
-
-      next(err);
-   }
-};
+   },
+   logger,
+   '@deleteBootcampById() [error: %s]'.red,
+);
 
 module.exports = {
    getBootcamps,
