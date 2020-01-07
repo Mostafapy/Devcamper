2d1a0c467aa59e03fe66ec101d856cf27cc18ccc
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-07 18:52:06 +0200

    fix asyncHandler to take logger for error

--- middlewares/asyncHandler.js
+++ middlewares/asyncHandler.js
@@ -1,5 +1,9 @@
 // eslint-disable-next-line implicit-arrow-linebreak
-const asyncHandler = fn => (req, res, next) =>
-   Promise.resolve(fn(req, res, next)).catch(next);
+const asyncHandler = (fn, logger, loggerMessage) => (req, res, next) =>
+   Promise.resolve(fn(req, res, next)).catch(err => {
+      err.loggerObject = { logger, loggerMessage };
+
+      next(err);
+   });
 
 module.exports = asyncHandler;
