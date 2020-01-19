7859f84fd1b6746583af0c86f34181a41c09bdd9
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-19 14:25:10 +0200

    add courses routers to index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRouters = require('express').Router();
 const bootcampsRouter = require('./bootcampsRoutes');
+const coursesRouter = require('./coursesRoutes');
 
 // Routes
 router.use('/api/v1', apiRouters);
 apiRouters.use('/bootcamps', bootcampsRouter);
+apiRouters.use('/courses', coursesRouter);
 
 module.exports = router;
