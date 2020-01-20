81f16a7bff40b5ef031729b44f0c5bb921dcf755
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-20 18:09:30 +0200

    add getAverageCostOfCourse helper function

---
+++ helpers/getAverageCostOfCourse.js
@@ -0,0 +1,26 @@
+require('colors');
+
+const logger = require('./../utils/logger')('Helpers:GetAverageCostOfCourse');
+
+// eslint-disable-next-line func-names
+module.exports = async function(bootcampId) {
+   try {
+      logger.log('Calculating avg cost ........'.blue);
+
+      const obj = await this.aggregate([
+         {
+            $match: { bootcamp: bootcampId },
+         },
+         {
+            $group: {
+               _id: '$bootcamp',
+               averageCost: { $avg: '$tuition' },
+            },
+         },
+      ]);
+
+      return Promise.resolve(obj);
+   } catch (err) {
+      logger.error('[error: %s]'.red, err.message);
+   }
+};
