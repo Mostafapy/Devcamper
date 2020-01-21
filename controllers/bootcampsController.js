5150c9654d2aeebc9f62c93c17d25da4d2c44475
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-21 16:07:48 +0200

    adding advancedResults middleware in bootcampsController

--- controllers/bootcampsController.js
+++ controllers/bootcampsController.js
@@ -11,80 +11,8 @@ const logger = require('./../utils/logger')('Controllers:BootcampsController');
 // @route GET /api/v1/bootcamps
 // @access Public
 const getBootcamps = asyncHandler(
-   async (req, res) => {
-      // Copy req.query
-      const reqQuery = { ...req.query };
-
-      // Fields to exclude
-      const removeFields = ['select', 'sort', 'page', 'limit'];
-
-      // Loop over removeFields array and delete them from reqQuery
-      // eslint-disable-next-line implicit-arrow-linebreak
-      removeFields.forEach(param => delete reqQuery[param]);
-
-      // Create query string
-      let queryStr = JSON.stringify(reqQuery);
-
-      // Create operator (gt, gte, etc)
-      queryStr = queryStr.replace(
-         /\b(gt|gte|lt|lte|in)\b/g,
-         // eslint-disable-next-line implicit-arrow-linebreak
-         match => `${match}`,
-      );
-
-      // Finding resources
-      let query = BootcampModel.find(JSON.parse(queryStr)).populate('courses');
-
-      // Select Fields
-      if (req.query.select) {
-         const fields = req.query.select.split(',').join(' ');
-         query = query.select(fields);
-      }
-
-      // Sort
-      if (req.query.sort) {
-         const sortBy = req.query.sort.split(',').join(' ');
-         query = query.sort(sortBy);
-      } else {
-         // Sort by date
-         query = query.sort('-createdAt');
-      }
-
-      // Pagination
-      const page = parseInt(req.query.page, 10) || 1;
-      const limit = parseInt(req.query.limit, 10) || 25;
-      const startIndex = (page - 1) * limit;
-
-      const endIndex = page * limit;
-
-      const total = await BootcampModel.countDocuments();
-
-      query.skip(startIndex).limit(limit);
-      // Executing query
-      const bootcamps = await query;
-
-      // Pagination result
-      const pagination = {};
-
-      if (endIndex > total) {
-         pagination.next = {
-            page: page + 1,
-            limit,
-         };
-      }
-      if (startIndex > 0) {
-         pagination.perv = {
-            page: page - 1,
-            limit,
-         };
-      }
-
-      res.status(200).json({
-         success: true,
-         count: bootcamps.length,
-         pagination,
-         data: bootcamps,
-      });
+   async (_req, res) => {
+      res.status(200).json(res.advancedResults);
    },
    logger,
    '@getBootcamps() [error: %s]'.red,
