bbb4edbafd024ceecb023ca84536e9871a19f925
MostafaHisham <mostafa.hisham@digi2.com>
2020-03-08 12:59:22 +0200

    adding auth router to main index routes file

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const apiRouters = require('express').Router();
 const bootcampsRouter = require('./bootcampsRoutes');
 const coursesRouter = require('./coursesRoutes');
+const authRouter = require('./authRoutes');
 
 // Routes
 router.use('/api/v1', apiRouters);
 apiRouters.use('/bootcamps', bootcampsRouter);
 apiRouters.use('/courses', coursesRouter);
+apiRouters.use('/auth', authRouter);
 
 module.exports = router;
