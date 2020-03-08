0db30cc372bebdaa09416813a19d45b3f0224d60
MostafaHisham <mostafa.hisham@digi2.com>
2020-03-08 16:50:07 +0200

    implement register controller in auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,32 @@
+/* eslint-disable object-curly-newline */
 require('colors');
 /* Models */
-// const UserModel = require('./../models/User');
+const UserModel = require('./../models/User');
 
 // const ErrorResponse = require('./../utils/errorResponse');
 const asyncHandler = require('./../middlewares/asyncHandler');
 
-// const logger = require('./../utils/logger')('Controllers:AuthController');
+const logger = require('./../utils/logger')('Controllers:AuthController');
 
 // @desc Register User
 // @route POST /api/v1/auth/register
 // @access Public
-const register = asyncHandler(async (_req, res) => {
-   res.status(200).json({ success: true });
-});
+const register = asyncHandler(
+   async (req, res) => {
+      const { name, email, password, role } = req.body;
+
+      // Create new user
+      const createdUser = await UserModel.create({
+         name,
+         email,
+         password,
+         role,
+      });
+
+      res.status(200).json({ success: true, data: createdUser });
+   },
+   logger,
+   '@register() [error: %s]'.red,
+);
 
 module.exports = { register };
