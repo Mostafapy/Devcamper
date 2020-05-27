dc3b5b011c5a540cd9fbd45f2cfb33a3dbfdebc4
MostafaHisham <mostafa.hisham@digi2.com>
2020-05-27 12:09:59 +0200

    add login controller in auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,7 @@ const UserModel = require('./../models/User');
 const asyncHandler = require('./../middlewares/asyncHandler');
 
 const logger = require('./../utils/logger')('Controllers:AuthController');
+const ErrorResponse = require('./../utils/errorResponse');
 
 // @desc Register User
 // @route POST /api/v1/auth/register
@@ -32,4 +33,53 @@ const register = asyncHandler(
    '@register() [error: %s]'.red,
 );
 
-module.exports = { register };
+// @desc Login User
+// @route POST /api/v1/auth/login
+// @access Public
+const login = asyncHandler(
+   async (req, res, next) => {
+      const { email, password } = req.body;
+
+      // Check for email and password
+      if (!email && !password) {
+         return next(
+            new ErrorResponse('please provide an email and password'),
+            400,
+            logger,
+            '@login() [error: please provide an email and password]'.red,
+         );
+      }
+
+      // Check for user
+      const user = await UserModel.findOne({ email }).select('+password');
+
+      if (!user) {
+         return next(
+            new ErrorResponse('Invalid Credentials'),
+            401,
+            logger,
+            '@login() [error: User is not found]'.red,
+         );
+      }
+
+      const isMatch = await UserModel.matchPassword(password);
+
+      if (!isMatch) {
+         return next(
+            new ErrorResponse('Invalid Credentials'),
+            401,
+            logger,
+            '@login() [error: Password provided is not the same in the database]'
+               .red,
+         );
+      }
+      // Create token
+      const token = UserModel.getSignedJwtToken();
+
+      res.status(200).json({ success: true, token });
+   },
+   logger,
+   '@login() [error: %s]'.red,
+);
+
+module.exports = { register, login };
