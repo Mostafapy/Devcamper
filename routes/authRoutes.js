c6a608bdeaf9fa00eceea20421a595289774ad8b
MostafaHisham <mostafa.hisham@digi2.com>
2020-06-01 10:50:08 +0200

    add new route to get current user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 /** Controllers */
-const { register, login } = require('../controllers/authController');
+const { register, login, getMe } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/register', register);
 
 router.post('/login', login);
+
+router.get('/me', getMe);
 
 module.exports = router;
