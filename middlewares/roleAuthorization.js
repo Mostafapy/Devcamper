b70aa1adc6d47b65b105c9188cb84d56acf96220
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-10-12 08:50:25 -0700

    add roleAuthorization middleware

---
+++ middlewares/roleAuthorization.js
@@ -0,0 +1,19 @@
+const ErrorResponse = require('./../utils/errorResponse');
+
+const logger = require('./../utils/logger')('Middlewares:RoleAuthorization');
+
+const roleAuthorization = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(res.user.role)) {
+            return next(
+                new ErrorResponse(`User role ${res.user.role} is Not authorized for that route`),
+                403,
+                logger,
+                '@roleAuthorization() [error: role is not authorized]'.red,
+             );
+        }
+        next();
+    }
+}
+
+module.exports = roleAuthorization;
