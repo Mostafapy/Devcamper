a31c5c13d0c9f3f4568160b61adb00e59b571769
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-10-14 08:25:44 -0700

    add user id instead of parameter url

--- middlewares/bootcampOwnership.js
+++ middlewares/bootcampOwnership.js
@@ -3,7 +3,7 @@ const asyncHandler = require("./asyncHandler");
 const bootcampOwnership = (bootcamp) => (req, res, next) => {
    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
     return next(new ErrorResponse(
-       `User ${req.params.id} is not authorized for this bootcamp`,
+       `User ${req.user.id} is not authorized for this bootcamp`,
        401,
        logger,
        '@bootcampOwnership() [error: %s]'.red,
