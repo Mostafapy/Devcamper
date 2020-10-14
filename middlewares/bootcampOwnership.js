a4135e50284e38a26cd754273fa97ae6d8bfb0dd
Mostafa Mohmed Hisham Ahmed Mostafa <mmham3919@gmail.com>
2020-10-14 08:15:43 -0700

    modify message for the bootcampOwnership middleware

--- middlewares/bootcampOwnership.js
+++ middlewares/bootcampOwnership.js
@@ -3,10 +3,10 @@ const asyncHandler = require("./asyncHandler");
 const bootcampOwnership = (bootcamp) => (req, res, next) => {
    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
     return next(new ErrorResponse(
-       `User ${req.params.id} is not authorized to update this bootcamp`,
+       `User ${req.params.id} is not authorized for this bootcamp`,
        401,
        logger,
-       '@updateBootcampById() [error: %s]'.red,
+       '@bootcampOwnership() [error: %s]'.red,
     ))
    }
 };
